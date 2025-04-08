f7168d2ea2bc90f3889b8eedd8c91e8e6a09dfb2
iqpht <204969342+iqpht@users.noreply.github.com>
2025-04-08 19:05:20 +0900

    add: fontzoom

---
+++ lua/custom/plugins/fontzoom.lua
@@ -0,0 +1,9 @@
+-- +/-でフォント拡大/縮小
+return {
+  'thinca/vim-fontzoom',
+  lazy = true,
+  keys = {
+    { '-', mode = 'n' },
+    { '+', mode = 'n' },
+  }
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,8 @@
 -- See the kickstart.nvim README for more information
 
 -->>>
+-- フォント
+vim.opt.guifont = 'Bizin Gothic Discord NF:h11'
 -- jjでインサートモードを抜ける
 vim.keymap.set('i', 'jj', '<ESC>')
 -- クリップボード貼り付け
