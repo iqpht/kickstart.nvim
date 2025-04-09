bbb2b9f8412a21f3b935a1cfd5aaab42075609d0
iqpht <204969342+iqpht@users.noreply.github.com>
2025-04-09 15:25:07 +0900

    edit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,13 +6,23 @@
 -->>>
 -- フォント
 vim.opt.guifont = 'Bizin Gothic Discord NF:h11'
+
 -- jjでインサートモードを抜ける
 vim.keymap.set('i', 'jj', '<ESC>')
+
 -- クリップボード貼り付け
 vim.keymap.set('n', '<Leader>p', '"+p', { desc = '[p]aste' })
 vim.keymap.set('v', '<Leader>p', '"+p', { desc = '[p]aste' })
+
 -- notes
 vim.keymap.set('n', '<Leader>mn', '<cmd>cd C:\\OneDrive\\notes|Telescope find_files<cr>', { desc = '[n]otes' })
+
+-- ビジュアルモードでのペースト時に、ヤンクしない
+vim.keymap.set('v', 'p', '"_dP')
+-- 1文字だけ削除したときに、ヤンクしない
+vim.keymap.set('n', 'x', '"_x')
+vim.keymap.set('n', 'X', '"_X')
+vim.keymap.set('n', 's', '"_s')
 --<<<
 
 return {}
