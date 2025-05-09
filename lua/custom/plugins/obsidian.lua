b33bde9fb4861a13c7da3653dadb39efd937879e
iqpht <204969342+iqpht@users.noreply.github.com>
2025-05-09 17:06:54 +0900

    edit

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,3 +1,4 @@
+-- obsidian
 return {
   'epwalsh/obsidian.nvim',
   version = '*',
--- lua/custom/plugins/skkeleton.lua
+++ lua/custom/plugins/skkeleton.lua
@@ -2,12 +2,18 @@
 -- scoop install deno (denoが必要)
 return {
   'vim-skk/skkeleton',
-  lazy = true,
+  lazy = false,
   keys = {
     { '<C-j>', '<Plug>(skkeleton-enable)', mode = { 'i', 'c' } },
+    { '<C-n>', '<cmd>call pum#map#insert_relative(+1)<CR>', mode = { 'i', 'c' } },
+    { '<C-p>', '<cmd>call pum#map#insert_relative(-1)<CR>', mode = { 'i', 'c' } },
   },
   dependencies = {
     'vim-denops/denops.vim',
+    'Shougo/ddc.vim',
+    'Shougo/ddc.vim',
+    'Shougo/pum.vim',
+    'Shougo/ddc-ui-pum',
   },
   config = function()
     vim.fn['skkeleton#config'] {
@@ -15,5 +21,22 @@ return {
       sources = { 'skk_dictionary', 'google_japanese_input' },
       eggLikeNewline = true,
     }
+
+    vim.cmd [[call ddc#custom#patch_global('ui', 'pum')]]
+    vim.cmd [[call ddc#custom#patch_global('sources', ['skkeleton'])]]
+    vim.cmd [[call ddc#custom#patch_global('sourceOptions', {
+        \ '_': {
+        \ 'matchers': ['matcher_head'],
+        \ 'sorters': ['sorter_rank'],
+        \ },
+        \ 'skkeleton' : {
+        \ 'mark': 'skkeleton',
+        \ 'matchers': [],
+        \ 'sorters': [],
+        \ 'converters': [],
+        \ 'isVolatile': v:true,
+        \ 'minAutoCompleteLength': 1
+        \ }})]]
+    vim.cmd [[call ddc#enable()]]
   end,
 }
